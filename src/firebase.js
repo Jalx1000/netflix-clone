fd296dff4a456a2d427356c16ba40f6894fa4ad5
Jal <jalejandromogro1@gmail.com>
2022-10-18 17:41:38 -0400

    setting

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4WIRNrAkVnasfg00Lz06YuIS4v9i02h0",
+    authDomain: "netflix-clone-react-f5e2f.firebaseapp.com",
+    projectId: "netflix-clone-react-f5e2f",
+    storageBucket: "netflix-clone-react-f5e2f.appspot.com",
+    messagingSenderId: "193103323651",
+    appId: "1:193103323651:web:8a5ab43c8a68e911bd2436"
+  };
