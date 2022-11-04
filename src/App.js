aa7e7b8595fdd20118a5a28c4e3ccc0e4151e15c
JAL <jalejandromogro1@gmail.com>
2022-11-04 00:46:36 -0400

    user redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = selectUser;//ojo aca useSelector(selectUser) no da
+  console.log(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser)=>{
-      if(authUser){
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
         //logged in
-        console.log(authUser)
-      }else{
+        console.log(userAuth);
+        dispatch(
+          login({
+            uuid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
         //logged out
+        dispatch(logout);
       }
     });
     return unsubscribe;
